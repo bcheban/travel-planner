fc8a2b121b1fd0be572e19c9bcdba058312c7d2c
bcheban <128683111+bcheban@users.noreply.github.com>
2025-07-17 21:44:07 +0300

    test

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,18 @@
-import {handlers} from "@/auth"
+import NextAuth from "next-auth"
+import GitHubProvider from "next-auth/providers/github"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/lib/prisma"  
 
-export const {GET, POST} = handlers
+const authOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
